4f0dcec8f024b1b016cd9dbcef1e1e8027c6ae9e
Lucas de Sá <lucasmdesa@usp.br>
2022-04-11 14:33:17 -0300

    add o=fe metallicity conversion and sample_histogram function

--- src/utils.py
+++ src/utils.py
@@ -11,24 +11,31 @@ def FeH_to_OFe(FeH):
     else:
         return -0.5 * FeH
 
+def FeH_to_OFe2(FeH):
+    return FeH
 
 def FeH_from_ZOH(FeH, ZOH):
     OFe = FeH_to_OFe(FeH)
     return FeH - ZOH + ZOH_SUN + OFe
 
+def FeH_from_ZOH2(FeH, ZOH):
+    OFe = FeH_to_OFe2(FeH)
+    return FeH - ZOH + ZOH_SUN + OFe
 
 def ZOH_from_FeH(FeH):
     OFe = FeH_to_OFe(FeH)
     return FeH + ZOH_SUN + OFe
 
-
 def ZOH_to_FeH(ZOH):
     [FeH] = fsolve(FeH_from_ZOH, np.array([-1]), args=ZOH)
     return FeH
 
+def ZOH_to_FeH2(ZOH):
+    [FeH] = fsolve(FeH_from_ZOH2, np.array([-1]), args=ZOH)
+    return FeH
 
 def FeH_to_Z(FeH):
-    return 10**FeH*0.02
+    return 10**FeH*Z_SUN
 
 
 def geoarange(a, b, n):
@@ -73,3 +80,34 @@ def interpolate(ipX, ipY, X):
         Y.append(f(X))
     Y = np.array(Y)
     return Y
+
+
+def sample_histogram(sample, N=1, m_min=0.08):
+    limits = [m_min]
+    csis = []
+
+    j = 0
+    for i, m in enumerate(sample):
+        j += 1
+        if j == N:
+            try:
+                limit = m / 2 + sample[i + 1] / 2
+            except IndexError:
+                limit = (3 / 2) * m - limits[-1] / 2
+            limits.append(limit)
+            j = 0
+
+    for i, m_i in enumerate(limits[:-1]):
+        m_iplus1 = limits[i + 1]
+        delta_m = m_iplus1 - m_i
+        csi = N / delta_m
+        csis.append(csi)
+
+    CSI_X = np.sort(np.append(limits, limits))
+
+    CSI_Y = np.array([2 * [csi] for csi in csis]).flatten()
+    CSI_Y = np.insert(CSI_Y, 0, 0)
+    CSI_Y = np.append(CSI_Y, 0)
+
+    CSI = np.array([CSI_X, CSI_Y]).T
+    return CSI
